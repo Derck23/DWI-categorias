32d9755fca935481711ec19c81ee67a077ec855b
Derck23 <eramireznieves25@gmail.com>
2025-08-01 01:24:55 -0600

    faltan pruebas

---
+++ src/main/java/mx/edu/uteq/idgs09_3/Controller/CategoriaController.java
@@ -0,0 +1,76 @@
+package mx.edu.uteq.idgs09_3.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+
+import mx.edu.uteq.idgs09_3.model.entity.Categorias;
+import java.util.List;
+import java.util.Optional;
+import mx.edu.uteq.idgs09_3.service.CategoriaService;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+
+
+
+@RestController 
+@RequestMapping("/api/categorias")
+public class CategoriaController {
+
+    @Autowired
+    private CategoriaService service;
+
+
+
+    @GetMapping()
+    public List<Categorias> buscarTodos(@RequestParam boolean soloActivo) {
+        return service.buscar(soloActivo);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Categorias> buscarPorId(@PathVariable int id) {
+        return service.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping()
+    public ResponseEntity<?> crear( @Validated @RequestBody Categorias c) {
+        Categorias entity = service.crear(c);
+        return ResponseEntity.ok(entity);
+    }
+
+    @PutMapping("/{id}") 
+    public ResponseEntity<?> editar(@PathVariable int id, @RequestBody Categorias entity) {
+        Optional<Categorias> optional = service.editar(id, entity);
+        if (optional.isPresent()) {
+            return ResponseEntity.ok(optional.get());
+    }
+    return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> eliminar(@PathVariable int id) {
+        if (service.borrar(id)) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ src/main/java/mx/edu/uteq/idgs09_3/service/CategoriaService.java
@@ -0,0 +1,61 @@
+package mx.edu.uteq.idgs09_3.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import mx.edu.uteq.idgs09_3.model.entity.Categorias;
+import mx.edu.uteq.idgs09_3.model.repository.CategoriaRepo;
+import mx.edu.uteq.idgs09_3.model.repository.TipoRequisitoRepo;
+import java.util.Optional;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepo repo;
+    @Autowired
+    private TipoRequisitoRepo trRepo;
+
+    @Transactional(readOnly = true)
+    public List<Categorias> buscar(boolean soloActivos) {
+        // Como ya no tenemos campo activo, simplemente devolvemos todas las categorías
+        return repo.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Categorias> buscarPorId(int id) {
+        return repo.findById(id);
+    }
+
+    @Transactional
+    public Categorias crear(Categorias categoria) {
+        return repo.save(categoria);
+    }
+
+    @Transactional
+    public Optional <Categorias> editar(int id, Categorias categoria) {
+        Optional<Categorias> opt = repo.findById(id);
+        if (opt.isPresent()) {
+            Categorias d = opt.get();
+            d.setNombre(categoria.getNombre());
+            d.setCategoriaPadre(categoria.getCategoriaPadre());
+            d.setCategoriaFederal(categoria.getCategoriaFederal());
+            d.setCategoriaEstatal(categoria.getCategoriaEstatal());
+            return Optional.of(repo.save(d));
+        }
+        return opt;
+    }
+
+    @Transactional
+    public boolean borrar(int id) {
+        Optional<Categorias> opt = repo.findById(id);
+        if (opt.isPresent()){
+            repo.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+}
